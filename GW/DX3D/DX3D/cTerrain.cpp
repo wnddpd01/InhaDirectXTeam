15ae1554d0e19674411aa8fd75112b413f7f5177
DESKTOP-RC8S5LR\User <a01057554609@gmail.com>
2020-10-21 17:48:12 +0900

    10/21 GW DX

---
+++ GW/DX3D/DX3D/cTerrain.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "cTerrain.h"
+
+
+cTerrain::cTerrain()
+{
+}
+
+
+cTerrain::~cTerrain()
+{
+}
+
+
---
+++ GW/DX3D/DX3D/cTerrain.h
@@ -0,0 +1,12 @@
+#pragma once
+class cTerrain
+{
+public:
+	cTerrain();
+	~cTerrain();
+private:
+
+public:
+	bool readRawFile(std::string filename);
+};
+
