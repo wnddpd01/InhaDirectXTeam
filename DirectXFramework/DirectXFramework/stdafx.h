e998aa025d823d37ccb96f6b40c14f3a9760f753
DESKTOP-RC8S5LR\User <choihh369@gmail.com>
2020-10-31 16:28:50 +0900

    Delete Singletone macro

--- DirectXFramework/DirectXFramework/stdafx.h
+++ DirectXFramework/DirectXFramework/stdafx.h
@@ -9,16 +9,4 @@
 #include <chrono>
 #include <cassert>
 
-//싱글턴 매크로
-#define Singletone(class_name) \
-	private : \
-		class_name(void) ; \
-		~class_name(void) ;	\
-	public : \
-		static class_name* GetInstance() \
-		{	\
-			static class_name instance; \
-			return &instance ; \
-		}
-
 #include "TimeManager.h"
