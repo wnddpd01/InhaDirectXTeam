c9300f6311530651d71979d3faaa07ee296c9735
DESKTOP-RC8S5LR\User <choihh369@gmail.com>
2020-10-31 17:16:45 +0900

    todo 추가

--- DirectXFramework/DirectXFramework/TimeManager.h
+++ DirectXFramework/DirectXFramework/TimeManager.h
@@ -13,7 +13,12 @@ class TimeManager
 	
 	public : 
 		static TimeManager* GetInstance() 
-		{	
+		{
+		/*
+			todo > GetInstance 인자로 enum 받아서 케이스처리
+			todo > 게임 시간처리 / 애니메이션용 시간 처리
+			todo > -시간 처리 : 최초는 스킵
+		*/
 			static TimeManager instance; 
 			return &instance ; 
 		}
