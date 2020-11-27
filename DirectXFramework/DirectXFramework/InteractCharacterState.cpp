a92edda4a570c774ef1418ea1e40c58253785975
UJae <kmj2942@naver.com>
2020-11-27 11:23:31 +0900

    플레이어 상호작용 물체 바라보게 수정

--- DirectXFramework/DirectXFramework/InteractCharacterState.cpp
+++ DirectXFramework/DirectXFramework/InteractCharacterState.cpp
@@ -22,6 +22,19 @@ void InteractCharacterState::Enter(Player& player)
 	LPD3DXANIMATIONSET curAnimSet;
 	player.GetSkinnedMesh()->m_pAnimController->GetTrackAnimationSet(0, &curAnimSet);
 	AnimationPeriod = curAnimSet->GetPeriod();
+
+	D3DXQUATERNION quatRot;
+	D3DXMATRIXA16 matRot;
+	D3DXVECTOR3 interactingObjectPos = player.GetInteractingObject()->GetPos();
+	interactingObjectPos.y = player.GetPos().y;
+
+	D3DXMatrixLookAtLH(&matRot, &(player.GetPos()), &interactingObjectPos, &D3DXVECTOR3(0, 1, 0));
+	D3DXMatrixInverse(&matRot, nullptr, &matRot);
+	D3DXQuaternionRotationMatrix(&quatRot, &matRot);
+
+	D3DXQUATERNION playerIdleRot;
+	D3DXQuaternionRotationYawPitchRoll(&playerIdleRot, D3DX_PI, 0, 0);
+	player.SetRot(playerIdleRot * quatRot);
 }
 
 CharacterState* InteractCharacterState::Update(Player& player)
--- DirectXFramework/DirectXFramework/MouseInputManager.h
+++ DirectXFramework/DirectXFramework/MouseInputManager.h
@@ -4,27 +4,11 @@
 
 #define gMouseInputManager MouseInputManager::GetInstance()
 
-struct isMouseState
-{
-	
-};
-
 class MouseInputManager : public Singleton<MouseInputManager>
 {
 public:
-	//BOOL DownLeftButton(POINT * const outPosition);
-	//BOOL DownRightButton(POINT * const outPosition);
-	//BOOL DownWheelButton(POINT * const outPosition);
-	//BOOL UpRightButton(POINT * const outPosition);
-	//BOOL UpLeftButton(POINT * const outPosition);
-	//BOOL DragLeftButton(POINT * const outPosition);
-	//BOOL DragRightButton(POINT * const outPosition);
-	//BOOL MoveMouse(POINT * const outPosition);
 	void WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-	//void Update();
 private:
-	//eEventName mMouseState;
-	//POINT prevMousePosition;
 	bool mbLButtonDown;
 	bool mbRButtonDown;
 };
