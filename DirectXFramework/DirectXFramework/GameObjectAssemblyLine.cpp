068a5ae9a75c8677daf385e3edb6dde1e64a4181
UJae <kmj2942@naver.com>
2020-12-27 21:04:40 +0900

    no message

--- DirectXFramework/DirectXFramework/GameObjectAssemblyLine.cpp
+++ DirectXFramework/DirectXFramework/GameObjectAssemblyLine.cpp
@@ -557,15 +557,6 @@ void GameObjectAssemblyLine::CreateIngameSceneGameObject(Scene* newScene)
 	portal2->SetExitPos(D3DXVECTOR3(25, 0, 123.5));
 	room2A02->InsertObject(portal2);
 
-	portalDraw = CreateStatic3DObject("portalDraw", "simplePlane.x",
-		D3DXVECTOR3(25, 2, 142.5),
-		D3DXVECTOR3(1, 2, 1),
-		D3DXVECTOR3(0, 0, 0)
-	);
-
-	portalDraw->SetTypeTag(eTypeTag::PORTAL);
-	newScene->mGameObjects.insert(make_pair("portalDraw2", portalDraw));
-
 	Interactable3DObject* door = new Interactable3DObject;
 	door->SetObjectName("door");
 	door->AddColliderCube("basicColliderCube");
